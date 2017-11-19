e1ebfc2ce9362ed8c7b0cdacaaeb79501834b817
ARTUR SEDRAKYAN <sedrakyan1998@gmail.com>
2017-11-19 16:48:28 +0400

    helloworld

---
+++ helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadLine();
+
+        }
+    }
+}
